cab83a845d6936c5a48bf3904f03446e5e998591
Pedro Rubin <prubincosta@gmail.com>
2021-10-18 01:26:25 -0300

    Initial commit

--- R/ler_pnadc_anual.R
+++ R/ler_pnadc_anual.R
@@ -1,40 +1,38 @@
-library(tidyverse)
-library(glue)
-library(readxl)
+
 
 ler_pnadc_anual <- function(ano, path_pnadc, path_leitores, path_deflator) {
   arquivo = glue("{path_pnadc}/PNADC_{ano}_visita1.txt")
   # salvamento = glue("./dados/anual/PNADC_{ano}.rds")
   dicionario = glue("{path_leitores}/leitores_{ano}.rds")
-  
+
   leitores <- read_rds(dicionario)
-  
+
   colpos <- fwf_widths(leitores$tamanho,
                        col_names = leitores$variavel)
   pnad <- read_fwf(file = as.character(arquivo),
                    col_positions = colpos,
                    col_types = cols(.default = col_character()))
-  
-  pnadx <- pnad %>% 
+
+  pnadx <- pnad %>%
     mutate(ID_dom = str_c(UPA,V1008,V1014),
            ID_pes = str_c(UPA,V1008,V1014,V2003),
-           ID_deflator = str_c(Ano,Trimestre,UF)) %>% 
+           ID_deflator = str_c(Ano,Trimestre,UF)) %>%
     mutate(across(.fns = as.numeric),
            regiao = floor(UF/10))
-  
-  deflator <- read_xls(glue("{path_deflator}/deflator_PNADC_2019.xls")) %>% 
+
+  deflator <- read_xls(glue("{path_deflator}/deflator_PNADC_2019.xls")) %>%
     mutate(Trimestre = trim,
            Ano = ano,
            UF = uf,
            Habitual = CO2,
            Efetivo = CO2e,
-           ID_deflator = str_c(Ano,Trimestre,UF)) %>% 
-    filter(Ano == unique(pnad$Ano)) %>% 
-    mutate(across(.fns = as.numeric)) %>% 
+           ID_deflator = str_c(Ano,Trimestre,UF)) %>%
+    filter(Ano == unique(pnad$Ano)) %>%
+    mutate(across(.fns = as.numeric)) %>%
     select(ID_deflator, Habitual, Efetivo)
-  
-  pnady <- pnadx %>% 
-    left_join(deflator, by = "ID_deflator") %>% 
+
+  pnady <- pnadx %>%
+    left_join(deflator, by = "ID_deflator") %>%
     mutate(VD4019_real = VD4019*Habitual,
            VD4020_real = VD4020*Efetivo,
            VD4048_real = VD4048*Efetivo)
